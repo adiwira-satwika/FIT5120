221fb8fe877272917debc590fd974ab456018294
Adiwira Satwika <adiwira@icloud.com>
2020-09-12 14:05:16 +1000

    Adding Quiz Models & Views

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,110 @@
+﻿using Environment_Green.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Environment_Green.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            return View();
+        }
+
+        // GET: Sample/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sample/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sample/Create
+        [HttpPost]
+        public ActionResult Create(FormOneViewModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                String states = nameof(model.States);
+                int occupants = model.Occupants;
+                double income = model.Income;
+                double energyBills = model.EnergyBills;
+                double waterBills = model.WaterBills;
+                String heaterType =Convert.ToString(model.HeaterTypes);
+                int heaterDuration = model.HeaterDuration;
+                String wmType = Convert.ToString(model.WMTypes);
+                int washingFrequency = model.WashingFrequency;
+                int room = model.Room;
+                String bulbType = Convert.ToString(model.BulbTypes);
+                int bulbDuration = model.BulbDuration;
+
+                ViewBag.States = states;
+
+                return RedirectToAction("Result");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sample/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Sample/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sample/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sample/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/SampleFormViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Environment_Green.Models
+{
+    public class SampleFormViewModel
+    {
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        public State States { get; set; }
+        public int Occupants { get; set; }
+        public double Income { get; set; }
+        [Display(Name = "Energy Bills")]
+        public double EnergyBills { get; set; }
+        public double WaterBills { get; set; }
+        public HeaterType HeaterTypes { get; set; }
+        public int HeaterDuration { get; set; }
+        public WMType WMTypes { get; set; }
+        public int WashingFrequency { get; set; }
+        public int Room { get; set; }
+        public BulbType BulbTypes { get; set; }
+        public int BulbDuration { get; set; }
+    }
+
+    public enum State
+        {
+            NSW, VIC, ACT, QLD, TAS, SA, NT, WA
+        }
+
+        public enum HeaterType
+        {
+            Gas, Electric, AC
+        }
+
+        public enum WMType
+        {
+            Front, Top
+        }
+
+        public enum BulbType
+        {
+            Converntional, LED
+        }
+}
